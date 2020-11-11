6fe8c18da94217dfbba1bcfd6ccf1dfc53a27180
HealthyJunkie <androwas101@gmail.com>
2020-11-11 09:35:01 +0200

    done handlebar excersize

--- main.js
+++ main.js
@@ -5,9 +5,12 @@ $("button").on("click", function () {
     let maxPrice = $("#max-p-input").val()
     let minRooms = $("#min-r-input").val()
     let maxRooms = $("#max-r-input").val()
+    let parking = $("#park-y")
     let immediate = $("#immed-y")
 
-    let relevantApts = findRelevantApts(address, minPrice, maxPrice, minRooms, maxRooms, immediate)
+    console.log(parking)
+
+    let relevantApts = findRelevantApts(address, minPrice, maxPrice, minRooms, maxRooms, immediate,parking)
     renderApts(relevantApts)
 })
 
@@ -15,6 +18,10 @@ const renderApts = function (apartments) {
     $("#results").empty()
     console.log(apartments) //array of apartments to render
     //Your code goes here.
+    const source = $('#apartments-template').html();
+    const template = Handlebars.compile(source)
+    const newHTML = template({apartments});
+    $('#results').append(newHTML);
 }
 
 renderApts(apartments) //renders apartments when page loads
